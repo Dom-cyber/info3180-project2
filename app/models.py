5cac9b362c188391a5056fc53fbb29a31eca74c5
Dominique Outar <dominiqueout1234@gmail.com>
2020-06-14 23:51:18 -0500

    a

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
