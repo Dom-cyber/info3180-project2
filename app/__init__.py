59359d9336fc868c6628a03ce5f584b0f9dcc156
Dominique Outar <dominiqueout1234@gmail.com>
2020-06-15 23:15:49 -0500

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
+import os
+import psycopg2
 from flask import Flask
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect 
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
-UPLOAD_FOLDER = './app/static/uploads'
+SQLALCHEMY_DATABASE_URI ="postgresql://project2:Temp1234@localhost/project2"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+UPLOAD_FOLDER = "./app/static/uploads"
+SECRET_KEY = 'Sup3r$3cretkey'
+
+app.config.from_object(__name__)
+csrf = CSRFProtect(app)
+db = SQLAlchemy(app)
+
+login = LoginManager(app) 
+login.init_app(app)
+login.login_view = 'login'
+
 from app import views
--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField, TextAreaField, SelectField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from flask import request
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FileField,SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from app.models import Users
-from flask import request
+
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	firstname = StringField('FirstName')
-	lastname = StringField('LastName')
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	location = StringField('Location', validators=[DataRequired()])
-	biography = TextAreaField('Biography', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	photo = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'Images only!'])])
+    username = StringField('Username', validators=[DataRequired()])
+    firstname= StringField('Firstname')
+    lastname= StringField('Lastname')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    location = StringField('Location', validators=[DataRequired()])
+    biography = TextAreaField('Biography', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    photo = FileField('Profile Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'Images only!'])])
+
+class PostForm(FlaskForm):
+    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg','png','jpeg', 'Images only!']) ])
+    caption = StringField('Caption', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    
+
+
+
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username invalid!.')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Username invalid!.')
 
 
